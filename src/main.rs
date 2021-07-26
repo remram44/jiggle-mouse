53577d1e1496850b0f5d3fe5d0f041171e8859a0
Remi Rampin <remi@rampin.org>
2021-07-26 12:16:49 -0400

    Add constants, print message

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,18 @@ use enigo::{Enigo, MouseControllable};
 use std::thread::sleep;
 use std::time::Duration;
 
+const PIXELS: i32 = 5;
+const INTERVAL: u64 = 50;
+const DURATION_MS: u64 = 100;
+
 fn main() {
     let mut enigo = Enigo::new();
+    println!("Moving mouse by {} pixels every {} seconds,", PIXELS, INTERVAL);
+    println!("until you close the window.");
     loop {
-        sleep(Duration::from_secs(58));
-        enigo.mouse_move_relative(5, 0);
-        sleep(Duration::from_millis(100));
-        enigo.mouse_move_relative(-5, 0);
+        sleep(Duration::from_secs(INTERVAL));
+        enigo.mouse_move_relative(PIXELS, 0);
+        sleep(Duration::from_millis(DURATION_MS));
+        enigo.mouse_move_relative(-PIXELS, 0);
     }
 }
